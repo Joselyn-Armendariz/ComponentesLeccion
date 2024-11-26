c79c36dffda47cf0ce50cb0635f338a25da1d2b9
Joselyn Armendariz <jossearmendariz19@gmail.com>
2024-11-26 17:08:02 -0500

    Leccion

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,51 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  constructor(private alertCtrl: AlertController) {}
 
-  constructor() {}
+  async onInicioClick() {
+    const alert = await this.alertCtrl.create({
+      backdropDismiss: false,
+      header: 'Inicio',
+      subHeader: 'Bienvenido',
+      message: 'Has presionado el botón Inicio.',
+      buttons: ['OK'],
+    });
+    await alert.present();
+  }
 
+  async onCancelarClick() {
+    const alert = await this.alertCtrl.create({
+      backdropDismiss: false,
+      header: 'Cancelar',
+      message: '¿Estás seguro de que quieres cancelar?',
+      buttons: [
+        {
+          text: 'Sí',
+          handler: () => {
+            console.log('Acción cancelada');
+          },
+        },
+        {
+          text: 'No',
+          role: 'cancel',
+          cssClass: 'rojo',
+          handler: () => {
+            console.log('Acción no cancelada');
+          },
+        },
+      ],
+    });
+    await alert.present();
+  }
+
+  async onHeartClick() {
+    const alert = await this.alertCtrl.create({
+      backdropDismiss: false,
+      header: 'Favorito',
+      message: 'Has añadido este elemento a tus favoritos.',
+      buttons: ['OK'],
+    });
+    await alert.present();
+  }
 }
